0f6a367efd9ebe7cdb59f2d44e69a60e2c5b781c
Franciely Aguilera <63638707+franxxav@users.noreply.github.com>
2020-04-16 15:03:59 +0200

    Add files via upload

--- Portafolio Nuevo/SCRIPTS/script.js
+++ Portafolio Nuevo/SCRIPTS/script.js
@@ -1,3 +1,42 @@
-window.onload=function(){
-    
-}
+window.onload = function() {
+  let Vermas1 = document.querySelectorAll(".Project1 .Vermas");
+  Vermas1[0].addEventListener("click", event => {
+    let ProjectSections = document.querySelectorAll(
+      ".Project1 .ProjectSection"
+    );
+    let ProjectIntro = document.querySelectorAll(".Project1 .ProjectText");
+    let ParentContainer = event.currentTarget.parentNode.parentNode.parentNode;
+    ParentContainer.style.height = "fit-content";
+    ProjectIntro[0].style.display = "none";
+    for (let i = 0; i < ProjectSections.length; i++) {
+      ProjectSections[i].style.display = "initial";
+    }
+  });
+  let Vermas3 = document.querySelectorAll(".Project3 .Vermas");
+  Vermas3[0].addEventListener("click", event => {
+    let ProjectSections = document.querySelectorAll(
+      ".Project3 .ProjectSection"
+    );
+    let ProjectIntro = document.querySelectorAll(".Project3 .ProjectText");
+    let ParentContainer = event.currentTarget.parentNode.parentNode.parentNode;
+    ParentContainer.style.height = "fit-content";
+    ProjectIntro[0].style.display = "none";
+    for (let i = 0; i < ProjectSections.length; i++) {
+      ProjectSections[i].style.display = "initial";
+    }
+  });
+
+  let Vermenos1 = document.querySelectorAll(".Project1 .VerMenos");
+  Vermenos1[0].addEventListener("click", event => {
+    let ProjectSections = document.querySelectorAll(
+      ".Project1 .ProjectSection"
+    );
+    let ProjectIntro = document.querySelectorAll(".Project1 .ProjectText");
+    let ParentContainer = event.currentTarget.parentNode.parentNode;
+    ParentContainer.style.height = "95vh";
+    ProjectIntro[0].style.display = "initial";
+    for (let i = 0; i < ProjectSections.length; i++) {
+      ProjectSections[i].style.display = "none";
+    }
+  });
+};
